32230a9dbea118fd75436c6a0f273a9fabbca56c
merwinthomas14 <merwinthomas14@gmail.com>
2025-09-27 10:32:14 +0530

    Create Footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Footer = () => (
+  <footer className="bg-gray-100 text-center p-4 mt-8">
+    <p>&copy; {new Date().getFullYear()} The Salwar Spot. All rights reserved.</p>
+  </footer>
+);
+
+export default Footer;
